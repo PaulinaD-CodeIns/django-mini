3725120ce4360973fe064c3ae46d52c8ed5d6fd7
Paulina Dolder <paulid4628@gmail.com>
2025-06-24 18:35:20 +0100

    Installed Whitenoise package

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x@x#+o__7(-=bnszj-&s9i-=xikjnu97u72t07)i8+nbfl_5jt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False 
+DEBUG = True 
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1',]
 
