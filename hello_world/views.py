8b065fffe1fbc18e43e1fd2f31cc55d8be81bfb9
Paulina Dolder <paulid4628@gmail.com>
2025-06-24 18:20:03 +0100

    Updated code ready for deployment to Heroku

--- hello_world/views.py
+++ hello_world/views.py
@@ -4,7 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(response):
-    if request.method == "POST":
-        return HttpResponse("You must have POSTed something")
-    else:
-        return HttpResponse(request.method)
+        return HttpResponse("Hello World!")
--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x@x#+o__7(-=bnszj-&s9i-=xikjnu97u72t07)i8+nbfl_5jt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1',]
 
 
 # Application definition
