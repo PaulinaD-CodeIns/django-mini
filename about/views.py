9fb41b27d9f2bb8ca4f2ad20519a83ce2a26ffee
Paulina Dolder <paulid4628@gmail.com>
2025-06-24 17:12:28 +0100

    Set up basic urls, views and apps

--- about/views.py
+++ about/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def index(response):
-    return HttpResponse("Hello wordlings!")
+def about_me(response):
+    return HttpResponse("About the wordlings!")
--- hello_world/apps.py
+++ hello_world/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class Section1Config(AppConfig):
+class Hello_WorldConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'section1'
+    name = 'hello_world'
