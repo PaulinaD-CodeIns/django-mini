0107496f1b5882019b55270a599f0a725e60e352
Paulina Dolder <paulid4628@gmail.com>
2025-06-25 13:09:05 +0100

    Hidden sensitive information in env.py

--- env.py
+++ env.py
@@ -1,5 +1,11 @@
 import os
 
+# Database URL
 os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:npg_xYpj43QBteUc@ep-bitter-frog-a23dtr86.eu-central-1.aws.neon.tech/prong_comic_flap_612587"
+)
+
+# Django Secret Key
+os.environ.setdefault(
+    "SECRET_KEY", "django-insecure-x@x#+o__7(-=bnszj-&s9i-=xikjnu97u72t07)i8+nbfl_5jt7"
 )
