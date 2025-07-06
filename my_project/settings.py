730120a142654baa78ad866a2d797a467d96fd5c
Paulina Dolder <paulid4628@gmail.com>
2025-07-06 21:14:02 +0100

    Installed pagination

--- my_project/settings.py
+++ my_project/settings.py
@@ -127,10 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles'), ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')     
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 
 
 # Default primary key field type
