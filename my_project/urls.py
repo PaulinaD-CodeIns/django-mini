2609183511aec0f6ce1da923071540b84ff6619d
Paulina Dolder <paulid4628@gmail.com>
2025-07-07 15:00:42 +0100

    Adding to save

--- my_project/urls.py
+++ my_project/urls.py
@@ -24,6 +24,7 @@ from about import views as about_views
 urlpatterns = [
     path("blog/", include("blog.urls"), name="blog-urls"),
     path('about/', about_views.about_me, name='about'),
+    path("accounts/", include("allauth.urls")),
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
 ]
